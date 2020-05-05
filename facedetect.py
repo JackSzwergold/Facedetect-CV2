32b3ae3229b627345af93e3e919308be23315a88
Jack Szwergold <JackSzwergold@gmail.com>
2020-05-04 22:31:48 -0400

    More work.

--- facedetect.py
+++ facedetect.py
@@ -54,6 +54,8 @@ PROFILES = {
     'HAAR_FRONTALFACE_DEFAULT': 'haarcascade_frontalface_default.xml',
     'HAAR_FRONTALFACE_ALT': 'haarcascade_frontalface_alt.xml',
     'HAAR_FRONTALFACE_ALT2': 'haarcascade_frontalface_alt2.xml',
+    'HAAR_PROFILEFACE': 'haarcascade_profileface.xml',
+    'HAAR_FULLBODY': 'haarcascade_fullbody.xml',
 }
 
 ################################################################################
