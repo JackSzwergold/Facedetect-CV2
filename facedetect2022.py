2b58b456e284854cf07869e818f3051c578fc04b
Jack Szwergold <JackSzwergold@gmail.com>
2023-09-06 19:21:01 -0400

    Adjustments to improve the facedetect script.

--- facedetect2022.py
+++ facedetect2022.py
@@ -109,8 +109,9 @@ def manage_face_detection(filename_full, biggest = False):
             crop_x = int(image_source.shape[1] / crop_factor)
             crop_y = int(image_source.shape[0] / crop_factor)    
             adjust_x = int(crop_x * (crop_factor - 1))
-            adjust_y = int(int(crop_y * (crop_factor - 1)) * 2)
+            adjust_y = int(crop_y * crop_factor)
             image_source = image_source[0:adjust_y, crop_x:adjust_x]
+            cv2.imwrite(filename + '_foo' + extension, image_source)
         image = cv2.addWeighted(image_source, contrast, image_source, 0, brightness)
         image = cv2.convertScaleAbs(image, alpha=contrast, beta=brightness)
         image = simplest_cb(image, 50)
